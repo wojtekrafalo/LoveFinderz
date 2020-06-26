72f2b3fa140a06d733ce43408386aab0d5ef7eae
wojtekrafalo <wojtekrafalo3@gmail.com>
2020-06-26 13:08:21 +0200

    Added configuration to Firebase connection.

---
+++ app/src/main/java/com/example/lovefinderz/di/FirebaseModule.kt
@@ -0,0 +1,20 @@
+package com.example.lovefinderz.di
+
+//TODO: Configure Dagger 2 injection.
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.database.FirebaseDatabase
+
+//import dagger.Module
+//import dagger.Provides
+//import javax.inject.Singleton
+//
+//@Module
+//@Singleton
+//class FirebaseModule {
+//
+//    @Provides
+//    fun firebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+//
+//    @Provides
+//    fun firebaseDatabase(): FirebaseDatabase = FirebaseDatabase.getInstance()
+//}
